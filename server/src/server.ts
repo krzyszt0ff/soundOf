dd035c46a525bca48366207d3b63d5e112a84a7b
Krzysztof <148912338+krzyszt0ff@users.noreply.github.com>
2026-09-04 16:28:45 +0200

    connected PostgreSQL to server (yay!!!!!!!!!!)

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,32 @@
 import express from 'express';
 import userRouter from './routes/user.js';
+import { PrismaClient } from './generated/prisma/client.js';
+import { PrismaPg } from '@prisma/adapter-pg';
+import 'dotenv/config'
+
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+export const prisma = new PrismaClient({ adapter });
 
 const app = express();
+app.use(express.json());
 
 app.use("/api/user", userRouter);
-
-app.use(express.json());
 
 app.get('/', (req, res) => {
   res.json({ message: 'SoundOf server is running a-ok!' });
 });
 
-const PORT = 4000;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+async function start() {
+    await prisma.$connect();
+    console.log('PostgreSQL (Prisma) connected');
+
+    const PORT = 4000;
+    app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+    });
+}
+
+start().catch((err) => {
+    console.error('Failed to start server:', err);
+    process.exit(1);
 });
